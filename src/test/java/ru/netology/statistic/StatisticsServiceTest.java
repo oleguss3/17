743aafd4ad50eb67b241c36c3028f89bc8fc8a3d
oleguss33 <oleguss0707@gmail.com>
2025-08-20 19:28:36 +0300

    сделан тест

---
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -0,0 +1,32 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatisticsServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMax_whenMaxGreaterThanFirst_triggersIfBranch() {
+        StatisticsService service = new StatisticsService();
+
+        // здесь есть элемент больше первого -> условие currentMax < income станет true
+        long[] incomesInBillions = {12, 5, 13, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 13;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
